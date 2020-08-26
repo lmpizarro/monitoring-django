2673a41e0ef8ec05e96f64d474cfe1aacc7df366
lmpizarro <lmpizarro@gmail.com>
2020-08-26 19:15:10 -0300

    init db cont

--- birras/app/management/commands/init_db.py
+++ birras/app/management/commands/init_db.py
@@ -6,16 +6,23 @@ import datetime
 class Command(BaseCommand):
     help = 'Init DB'
 
-    meeters = [('mario@santader.com', 'mario'),
-               ('rosa@alba.com', 'rosa'),
-               ('ana@gmail.com', 'ana')]
+    meeters = [['mario@santader.com', 'mario'],
+               ['rosa@alba.com', 'rosa'],
+               ['ana@gmail.com', 'ana']]
 
-    meetups =[
-        ('anual meeting', datetime.datetime(2020, 10, 9, 18, 0, 0, 0), 'the place', 'nice anual meeting'),
-        ('Reunion Barrio', datetime.datetime(2020, 11, 9, 21, 0, 0, 0), 'the other place', 'la reunion de barrio')
+    meetups = [
+        ['anual meeting', datetime.datetime(2020, 10, 9, 18, 0, 0, 0), 'the place', 'nice anual meeting'],
+        ['Reunion Barrio', datetime.datetime(2020, 11, 9, 21, 0, 0, 0), 'the other place', 'la reunion de barrio']
     ]
 
     def handle(self, *args, **options):
+
+        try:
+            Meeter.objects.all().delete()
+            MeetUP.objects.all().delete()
+        except Exception as e:
+            print('ERROR DELETE DB')
+
         for m in self.meeters:
             email = m[0]
             name = m[1]
@@ -23,6 +30,7 @@ class Command(BaseCommand):
             try:
                 model:Meeter = Meeter(email=email, name=name)
                 model.save()
+                m.append(model)
             except Exception as e:
                 print(f'Error {e}')
 
@@ -30,6 +38,14 @@ class Command(BaseCommand):
             try:
                 model: MeetUP = MeetUP(name=m[0], meet_date=m[1], place=m[2], description=m[3])
                 model.save()
+                m.append(model)
             except Exception as e:
                 print(f'Error {e}')
 
+        # all meeters to meetup[0], meeter 0 y 2 meetup 1
+        self.meetups[0][4].meeters.add(self.meeters[0][2])
+        self.meetups[0][4].meeters.add(self.meeters[1][2])
+        self.meetups[0][4].meeters.add(self.meeters[2][2])
+
+        self.meetups[1][4].meeters.add(self.meeters[0][2])
+        self.meetups[1][4].meeters.add(self.meeters[2][2])
