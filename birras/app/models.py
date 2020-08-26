c617715cae2b6fd2cb76540217ff1b1199ce5916
lmpizarro <lmpizarro@gmail.com>
2020-08-26 16:44:23 -0300

    meetup model init

--- birras/app/models.py
+++ birras/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class MeetUP(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    meet_date = models.DateTimeField('meet up date', null=True)
+    place = models.TextField('The Place', max_length=512, null=True)
+    description = models.TextField('Brief', max_length=512, null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.meet_date}'
+
+
+
