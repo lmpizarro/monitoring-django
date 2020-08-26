9994146903bce15263e5e2c781d03d2f52f0f4c0
lmpizarro <lmpizarro@gmail.com>
2020-08-26 12:53:08 -0300

    correct temperature by kelvin

--- birras/app/services.py
+++ birras/app/services.py
@@ -11,6 +11,10 @@ class Weather():
         self.endpoint_weather = settings.WEATHER_API_URL.format(settings.WEATHER_PLACE, 
                                                                 settings.WEATHER_APPID)
         
+    @staticmethod
+    def to_celsius(temp):
+        return float('{:.2f}'.format(temp - 273.15))
+        
     def call_weather_api(self):                
         try:     
             response = requests.get(self.endpoint_weather)
@@ -18,7 +22,11 @@ class Weather():
             data = data['main']
             data.pop('pressure', None)
             data.pop('humidity', None)
-            data['error'] = False            
+            data['error'] = False
+            data['temp'] = Weather.to_celsius(data['temp'])
+            data['temp_min'] = Weather.to_celsius(data['temp_min'])
+            data['temp_max'] = Weather.to_celsius(data['temp_max'])
+            data['feels_like'] = Weather.to_celsius(data['feels_like'])                                    
         except Exception as e:
             message = 'ERROR: {}'.format(e)
             data = {}
