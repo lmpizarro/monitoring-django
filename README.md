0437bb6a70446b2e7b445cafb5fa08a8d91fe041
lmpizarro <lmpizarro@gmail.com>
2020-08-26 11:29:02 -0300

    get weather skeleton

--- README.md
+++ README.md
@@ -39,4 +39,4 @@ Apis weather:
 
 python3 -m venv venv
 source venv/bin/activate
-
+pip install -r requirements.txt
--- birras/app/views.py
+++ birras/app/views.py
@@ -7,9 +7,25 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 
-class HelloView(APIView):
+class HelloBeerService(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        content = {'message': 'Hello, World!'}
+        content = {'message': 'Hello, I am the Beer Service!'}
         return Response(content)
+        
+
+class GetWeatherTemperature(APIView):
+
+    def call_weather_api():
+        return 24
+        
+    def get(self, request):
+        # call weather api
+        
+        temperature = call_weather_api()
+        
+        content = {'temperature': f'{temperature}'}
+        return Response(content)
+
+    
