65352531cc6045293dea898e06edccaadae6a99e
lmpizarro <lmpizarro@gmail.com>
2020-08-26 15:50:45 -0300

    drf-yasg added swagger

--- birras/app/urls.py
+++ birras/app/urls.py
@@ -1,8 +1,30 @@
 from django.urls import path
 from app import views
 
+from django.conf.urls import url
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+ 
 urlpatterns = [
     path('hello/', views.HelloBeerService.as_view(), name='hellobeer'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('temperature/', views.GetWeatherTemperature.as_view(), name='gettemperature'),
     path('bottlesByPerson/', views.GetBottlesByPerson.as_view(), name='bottleByPerson'),
 ]
--- birras/birras/urls.py
+++ birras/birras/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
